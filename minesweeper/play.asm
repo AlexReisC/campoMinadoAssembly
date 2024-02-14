978bf6f1a9fe63cb8324692a69bf2d58ff0bed06
AlexReisC <alex.reis@aluno.ufca.edu.br>
2024-02-14 17:52:17 -0300

    adiciona o play

--- minesweeper/play.asm
+++ minesweeper/play.asm
@@ -7,29 +7,32 @@ play:
 	move $s0, $a0 # board
 	move $s1, $a1 # row
 	move $s2, $a2 # colunm
+	
 	sll $t0, $s1, 5 # row*4*SIZE
 	sll $t1, $s2, 2 # colunm*4
 	add $t0, $t0, $t1 
 	add $t0, $t0, $s0
-	sw $t0, 0($t0)
-	li $t1, -1
-	beq $t0, $t1, end_play_hit_bomb
+	lw $t1, 0($t0)
+	li $t2, -1
+	beq $t1, $t2, hit_bomb
 	
-	addi $t1, $t1, -1
-	bne $t0, $t1, continue_game
+	li $t2, -2
+	bne $t1, $t2, entry_if
 	
-	# x = countAdjacentBoms(board, row, colunm)
-	jal countAdjacentBombs
-	move $s3, $v0
-	# if(!x)
-	bne $s3, $zero, continue_game
-	jal revealNeighboringCells
+	hit_bomb:
+		move $v0, $zero
+		jr $ra
+
+	entry_if:
+		# x = countAdjacentBoms(board, row, colunm)
+		jal countAdjacentBombs
+		move $s3, $v0
+		sw $s3, 0($t0) # board[row][colunm] = x
+		bne $s3, $zero, x_no_zero_p
 	
-	end_play_hit_bomb:
-	move $v0, $zero
-	jr $ra
+	x_no_zero_p:
+		jal revealNeighboringCells # revealNeighboringCells(board, row, colunm)
 	
-	continue_game:
 	li $t3, 1
-	move $v0, $t3 
+	move $v0, $t3 # return 1
 	jr $ra
