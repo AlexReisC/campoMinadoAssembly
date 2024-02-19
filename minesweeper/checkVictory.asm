1473f76cb1e76d42963b3990f997b9f007343962
AlexReisC <alex.reis@aluno.ufca.edu.br>
2024-02-18 23:14:07 -0300

    termina o check victory

--- minesweeper/checkVictory.asm
+++ minesweeper/checkVictory.asm
@@ -1,35 +1,45 @@
 .include "macros.asm"
 
 .globl checkVictory
-#AVISO: ESSE ARQUIVO N VEIO COMO CODIGO BASE
 
 checkVictory:
+	save_context
 
-save_context
-
-li $s3,0 # int count = 0
-move $s0, $a0 # $s0 = board
-li $s1,0 		# i = 0
+	li $s1,0 # int count = 0
+	move $s0, $a0 # $s0 = board
+	li $s2,0 		# i = 0
 
 #primeiro loop
-begin_for_i_it: # for (int i = 0; i < SIZE; ++i)
-
-li $t0,SIZE  #size é o tamanho do board
-bge $s1,$t0,end_for_i_it #s1 <= t0 continua repetindo
+	begin_for_i_cv: # for (int i = 0; i < SIZE; ++i)
+		li $t0,SIZE  #size ï¿½ o tamanho do board
+		bge $s2,$t0,end_for_i_cv #s1 <= t0 continua repetindo
 
 #segundo loop
-li $s2,0    # j = 0
-begin_for_j_it:
-li $t0,SIZE
-bge $s2,$t0,end_for_j_it
+	li $s3,0    # j = 0
+	begin_for_j_cv:
+		li $t0,SIZE
+		bge $s3,$t0,end_for_j_cv
 #codigo do interior dos 2 loops
-
-bltz  #board  aumenta_count
-
-j begin_for_i_it
-
-aumenta_count:
-addi $s3,$s3,1 #aumentar count
-
-
+	
+	addi $s3, $s3, 1
+	j begin_for_j_cv
+	end_for_j_cv:
+		addi $s2, $s2, 1
+	
+	j begin_for_i_cv
+	end_for_i_cv:
+		li $t0, SIZE
+		li $t1, SIZE
+		mult $t0, $t1 # SIZE * SIZE
+		mflo $t2
+		li $t1, BOMB_COUNT
+		sub $t0, $t2, $t1 # SIZE * SIZE - BOM_COUNT
+		slt $t3, $s1, $t0
+		beq $t3, $zero, victory
+		li $v0, 0 # return 0
+		jr $ra
+	
+	victory:
+		li $v0, 1 # return 1
+		jr $ra
 
